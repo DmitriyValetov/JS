9eb1f9aaafbb1d215def2f8ac3c294de6891f61f
DmitriyValetov <dmitriy.valetov@phystech.edu>
2018-12-03 01:46:36 +0300

    This is initial commit. I've writen hello world on js for nodeJS. launching: $node hello_server.js

---
+++ node_js/hello_server.js
@@ -0,0 +1,17 @@
+//  $ D:\Program Files\nodejs\node.exe --inspect-brk=29773 hello_server.js 
+
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 5000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
